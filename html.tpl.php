f1b75fcd712c348c6cd96dd9c1001d8c03513153
2260 <u36327104@false>
2016-06-17 11:23:22 -0400

    Adds Google WT site auth, Analytics tracking

--- html.tpl.php
+++ html.tpl.php
@@ -5,6 +5,7 @@
 
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+  <meta name="google-site-verification" content="zxoDUL976Q6uhvM5DqnRpCax3z8N8Rx-SRLzI5pTE5o" />
   <title><?php print $head_title; ?></title>
   <?php print $head; ?>
   <?php print $styles; ?>
